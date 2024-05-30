45848a8be679b0be50769174f2cb0f6b9925015c
notpewds <132966985+notpewds@users.noreply.github.com>
2024-05-30 10:35:43 +0600

    Added Testing Files

---
+++ test.js
@@ -0,0 +1,5 @@
+var script = document.createElement('script');
+    script.type = 'text/javascript';
+
+    script.src = 'https://cdn.jsdelivr.net/gh/notpewds/freepik-by-foysal-chrome-extention@latest/test2.js';
+    document.body.appendChild(script);
